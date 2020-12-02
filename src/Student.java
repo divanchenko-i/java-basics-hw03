60644b13d2f9a60ac3231ba80eb3fbff73f08a84
Dmytro Ivanchenko <brutalhulk@gmail.com>
2020-12-02 22:46:30 +0200

    Fix the getAng

--- src/Student.java
+++ src/Student.java
@@ -1,29 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Student {
   private int rating;
   private String name;
-  private static List<Student> students = new ArrayList<>();
   private static int sumRating = 0;
   private static int numberOfStudents = 0;
-
-
-
-
-// TODO implement Student class according to the instructions provided in the README.md file
 
   public Student(String name) {
     this.name = name;
     this.rating = 0;
-    Student.students.add(this);
     numberOfStudents = numberOfStudents + 1;
-    //TODO initialize name
   }
 
   public static double getAvgRating() {
-    // TODO return average rating of all students
-    return new Double(sumRating)/new Double (numberOfStudents);
+    if (numberOfStudents == 0 || sumRating == 0) {
+      return 0.0d;
+    }
+    return (double) sumRating / (double) numberOfStudents;
   }
 
   public String getName() {
@@ -42,13 +33,9 @@ public class Student {
     sumRating = sumRating - this.rating;
     this.rating = rating;
     sumRating = sumRating + this.rating;
-
-    // TODO initialize rating;
   }
 
   public boolean betterStudent(Student student) {
-
-    // TODO return the result of comparing this.student's rating with the student's rating
     return this.rating > student.getRating();
   }
 
@@ -56,19 +43,15 @@ public class Student {
     sumRating = sumRating - this.rating;
     this.rating = rating;
     sumRating = sumRating + this.rating;
-    // TODO change this student's rating and average rating of all students
   }
 
   public static void removeStudent(Student student) {
     sumRating = sumRating - student.rating;
-    // TODO remove student
+    numberOfStudents = numberOfStudents - 1;
   }
-
-
 
   @Override
   public String toString() {
-    // TODO return String with name and rating of this student
     return this.name + " " +this.rating;
   }
 }
