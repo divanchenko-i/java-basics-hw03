937d95189ebb453d91a0b49a39d74c4e65a7b276
Dmytro Ivanchenko <brutalhulk@gmail.com>
2020-12-02 20:48:07 +0200

    Create the app for the unit test, but with some stupid bugs

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,29 @@
-public class Student {
-  int rating;
-  private String name;
+import java.util.ArrayList;
+import java.util.List;
 
-  // TODO implement Student class according to the instructions provided in the README.md file
+public class Student {
+  private int rating;
+  private String name;
+  private static List<Student> students = new ArrayList<>();
+  private static int sumRating = 0;
+  private static int numberOfStudents = 0;
+
+
+
+
+// TODO implement Student class according to the instructions provided in the README.md file
 
   public Student(String name) {
+    this.name = name;
+    this.rating = 0;
+    Student.students.add(this);
+    numberOfStudents = numberOfStudents + 1;
     //TODO initialize name
   }
 
   public static double getAvgRating() {
     // TODO return average rating of all students
-    return 0;
+    return new Double(sumRating)/new Double (numberOfStudents);
   }
 
   public String getName() {
@@ -18,7 +31,7 @@ public class Student {
   }
 
   public void setName(String name) {
-    // TODO set student's name
+    this.name = name;
   }
 
   public int getRating() {
@@ -26,25 +39,36 @@ public class Student {
   }
 
   public void setRating(int rating) {
+    sumRating = sumRating - this.rating;
+    this.rating = rating;
+    sumRating = sumRating + this.rating;
+
     // TODO initialize rating;
   }
 
   public boolean betterStudent(Student student) {
+
     // TODO return the result of comparing this.student's rating with the student's rating
-    return false;
+    return this.rating > student.getRating();
   }
 
   public void changeRating(int rating) {
+    sumRating = sumRating - this.rating;
+    this.rating = rating;
+    sumRating = sumRating + this.rating;
     // TODO change this student's rating and average rating of all students
   }
 
   public static void removeStudent(Student student) {
+    sumRating = sumRating - student.rating;
     // TODO remove student
   }
+
+
 
   @Override
   public String toString() {
     // TODO return String with name and rating of this student
-    return "";
+    return this.name + " " +this.rating;
   }
 }
